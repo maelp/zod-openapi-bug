757423c8a906eee73005af030da6ca4d5994a1c1
Mael Primet <mael.primet@gmail.com>
2025-08-01 15:35:46 +0200

    feat: add comment with the hack

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,3 +1,9 @@
+// UNCOMMENT THOSE LINES FOR A HACK WHICH SOLVES THE ISSUE
+// import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
+// import { z } from "zod/v4";
+// // HACK: we add this for zod-openapi, so that it finds the .openapi even when starting with `drizzle-kit generate`, etc
+// extendZodWithOpenApi(z);
+
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
